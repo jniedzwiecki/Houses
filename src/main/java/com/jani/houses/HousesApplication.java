ba5556b506d70e462b5e768410e2101f9179b07a
Jacek Niedźwiecki <jacek.niedzwiecki@tomtom.com>
2018-05-22 00:18:12 +0200

    Added more columns to database

--- src/main/java/com/jani/houses/HousesApplication.java
+++ src/main/java/com/jani/houses/HousesApplication.java
@@ -10,10 +10,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import static com.jani.houses.Teaser.teaser;
 import static io.vavr.API.Try;
@@ -21,9 +20,13 @@ import static io.vavr.API.Try;
 @SpringBootApplication
 public class HousesApplication {
 
+    private static final String DATABASE = "jdbc:mysql://localhost/houses?serverTimezone=CET";
+
     private static final Logger logger = LoggerFactory.getLogger(HousesApplication.class);
+
     private static final String ID = "id";
     private static final String TITLE = "title";
+    private static final String URL = "href";
     private static final String A_TEASER_CLASS = "a[class^=teaser]";
     private static final String DIV_PAGINATION_CLASS = "div[class=pagination]";
     private static final String GRATKA = "https://gratka.pl/nieruchomosci/domy?rodzaj-ogloszenia=sprzedaz&lokalizacja_region=%C5%82%C3%B3dzkie&lokalizacja_miejscowosc=lodz&lokalizacja_dzielnica=polesie&page=";
@@ -34,15 +37,12 @@ public class HousesApplication {
 //        SpringApplication.run(HousesApplication.class, args);
 
         maxPageIndex()
-            .peek(maxIndex -> {
-                    Stream<Teaser> teasers = Stream.rangeClosed(1, maxIndex)
-                        .map(page -> GRATKA + page)
-                        .flatMap(HousesApplication::extractTeasersForPage);
-
-                    teasers
-                        .map(HousesApplication::updateTeaser)
-                        .forEach(Try::get);
-                }
+            .peek(maxIndex ->
+                Stream.rangeClosed(1, maxIndex)
+                    .map(page -> GRATKA + page)
+                    .flatMap(HousesApplication::extractTeasersForPage)
+                    .map(HousesApplication::updateTeaser)
+                    .forEach(Try::get)
             )
             .getOrElseThrow(() -> new IllegalStateException("Could not download main document."));
 
@@ -70,24 +70,26 @@ public class HousesApplication {
     }
 
     private static Stream<Teaser> extractTeasers(Elements teaserElements) {
-        java.util.stream.Stream<Teaser> teaserStream =
-            teaserElements.stream().map(teaser -> teaser(teaser.attr(ID), teaser.attr(TITLE)));
-        return Stream.ofAll(teaserStream);
+        return Stream.ofAll(teaserElements.stream())
+            .map(teaser -> teaser(teaser.attr(ID), teaser.attr(TITLE), teaser.attr(URL)));
     }
 
     private static Try<Boolean> updateTeaser(Teaser teaser) {
         logger.info("Storing teaser id={} into db", teaser.id());
 
-        String myUrl = "jdbc:mysql://localhost/houses?serverTimezone=UTC";
-
         return
-            Try.withResources(() -> DriverManager.getConnection(myUrl, "houses", "eif(4es2"))
+            Try.withResources(() -> DriverManager.getConnection(DATABASE, "houses", "eif(4es2"))
                 .of(conn -> {
-                    String query = "insert into offers (offer_id, title) values (?, ?)";
+                    String query = "insert into offers (offer_id, title, visited_time, url) "
+                        + "values (?, ?, ?, ?) "
+                        + "on duplicate key update visited_time = ?";
 
                     PreparedStatement preparedStmt = conn.prepareStatement(query);
                     preparedStmt.setString(1, teaser.id());
                     preparedStmt.setString(2, teaser.title());
+                    preparedStmt.setTimestamp(3, Timestamp.from(new java.util.Date().toInstant()));
+                    preparedStmt.setString(4, teaser.url());
+                    preparedStmt.setTimestamp(5, Timestamp.from(new java.util.Date().toInstant()));
 
                     return preparedStmt.execute();
                 });
--- src/main/java/com/jani/houses/Teaser.java
+++ src/main/java/com/jani/houses/Teaser.java
@@ -4,26 +4,32 @@ public class Teaser {
 
     private final String id;
     private final String title;
+    private final String url;
 
-    static Teaser teaser(String id, String title) {
-        return new Teaser(id, title);
+    static Teaser teaser(String id, String title, String url) {
+        return new Teaser(id, title, url);
     }
 
-    private Teaser(String id, String title) {
+    private Teaser(String id, String title, String url) {
         this.id = id;
         this.title = title;
+        this.url = url;
     }
 
-    public String id() {
+    String id() {
         return id;
     }
 
-    public String title() {
+    String title() {
         return title;
+    }
+
+    public String url() {
+        return url;
     }
 
     @Override
     public String toString() {
-        return String.format("ID: %s; title: %s", id, title);
+        return String.format("ID: %s; title: %s, url: %s", id, title, url);
     }
 }
