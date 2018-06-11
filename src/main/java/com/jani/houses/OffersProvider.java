cec25270a3060b01a0069e3377c7f5d9f37f6ba5
Jacek Niedźwiecki <jacek.niedzwiecki@tomtom.com>
2018-06-11 10:01:03 +0200

    Adding other providers

--- src/main/java/com/jani/houses/OffersProvider.java
+++ src/main/java/com/jani/houses/OffersProvider.java
@@ -2,14 +2,11 @@ package com.jani.houses;
 
 enum OffersProvider {
 
-    GRATKA("https://gratka.pl/nieruchomosci/domy?rodzaj-ogloszenia=sprzedaz&lokalizacja_region=%C5%82%C3%B3dzkie" +
-        "&lokalizacja_miejscowosc=lodz&lokalizacja_dzielnica=polesie&page=") {
-
-        @Override
-        public String pageNumberToUrl(int number) {
-            return baseUrl + number;
-        }
-    };
+    GRATKA("https://gratka.pl/nieruchomosci/domy?rodzaj-ogloszenia=sprzedaz&lokalizacja_region=%C5%82%C3%B3dzkie"
+        + "&lokalizacja_miejscowosc=lodz&lokalizacja_dzielnica=polesie&page="),
+    OTODOM("https://www.otodom.pl/sprzedaz/dom/lodz/polesie/?search%5Bdescription%5D=1&search%5Bdist%5D=0"
+        + "&search%5Bdistrict_id%5D=112&search%5Bsubregion_id%5D=127&search%5Bcity_id%5D=1004&page="),
+    OLX("https://www.olx.pl/nieruchomosci/domy/lodz/?search%5Bdistrict_id%5D=295&page=");
 
     final String baseUrl;
 
@@ -21,5 +18,7 @@ enum OffersProvider {
         return pageNumberToUrl(1);
     }
 
-    public abstract String pageNumberToUrl(int number);
+    public String pageNumberToUrl(int number) {
+        return baseUrl + number;
+    }
 }
